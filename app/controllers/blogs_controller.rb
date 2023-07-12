a3001f556e5aeacb5eb70384e564755ff33b25fd
keiki <kkeki0410@gmail.com>
2023-07-12 18:49:41 +0900

    7/12まで

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,59 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
+    @blogs = Blog.all
+  end
+  def index
+    @blogs = Blog.all
   end
 
-  # 以下を追記する
   def new
     @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save    
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+    # @blog = Blog.find(params[:id])
+  end
+
+  def edit
+    # @blog = Blog.find(params[:id])
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
+  def update
+    # @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def confirm
+      @blog = Blog.new(blog_params)
+  end
+  
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
